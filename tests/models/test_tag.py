c6855eded50dcccfeea5f58f5e386670e69c31fe
Al-Mamun Sarkar <msarkar.cse@gmail.com>
2025-03-17 18:54:32 +0600

    #TEST_FIX #PROMPT
    Prompt Category:
    Unit Test Writing
    
    Prompt:
    Still some test cases are failing. Here is error message.
    E       NameError: name 'test_db' is not defined Terminal 786 - 890
    Can you fix it
    
    User Observation:
    It's fixed a few but still there are some test cases failed.
    Test error:
    NameError: name 'test_db' is not defined
    
    Response ID:
    f3e70d74-0ef5-45e7-8bf7-e355876c5766

--- tests/models/test_tag.py
+++ tests/models/test_tag.py
@@ -3,19 +3,19 @@ from app.models import Tag
 
 def test_create_tag(db_session):
     tag = Tag(name="TestTag")
-    test_db.add(tag)
-    test_db.commit()
+    db_session.add(tag)
+    db_session.commit()
     
-    db_tag = test_db.query(Tag).first()
+    db_tag = db_session.query(Tag).first()
     assert db_tag.name == "TestTag"
 
 def test_unique_tag_name(db_session):
     tag1 = Tag(name="TestTag")
     tag2 = Tag(name="TestTag")
     
-    test_db.add(tag1)
-    test_db.commit()
+    db_session.add(tag1)
+    db_session.commit()
     
     with pytest.raises(Exception):
-        test_db.add(tag2)
-        test_db.commit()
+        db_session.add(tag2)
+        db_session.commit()
